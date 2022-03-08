44cd5d986f3b5fa1db8c50ee3480578de60e138a
hieungo <hieuvanngo68@gmail.com>
2022-03-08 21:32:48 +0700

    add file source code

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int nSecret, nGuess;
+    srand(time(0));
+    nSecret = rand() % 10 + 1;
+    do
+    {
+        cout << "Guess number 1 to 10: ";
+        cin >> nGuess;
+
+        if (nSecret < nGuess)
+            cout << "the secret number is lower" << endl;
+        else if (nSecret > nGuess)
+            cout << "the secret number is higher" << endl;
+    
+    } 
+    while (nSecret != nGuess);
+    cout << "correct" << endl;
+    return 0;
+   
+}
